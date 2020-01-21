9944a0117ef30b74bf72a72fb121d634d4749124
Norvil87 <50398225+Norvil87@users.noreply.github.com>
2020-01-21 21:46:29 +0300

    Исправлены замечания по ревью

--- build/js/main.js
+++ build/js/main.js
@@ -1,34 +1,36 @@
 'use strict';
 
-const KeyCodes = {
+var KeyCodes = {
   ENTER: 'Enter',
   ESC: 'Escape'
 };
 
-const Resolutions = {
+var Resolutions = {
   MOBILE_MAX: 767
 };
 
-const TEL_LENGTH = 14;
+var TEL_LENGTH = 14;
 
-let popupButton = document.querySelector('.header__user-button');
-let modalUnderlay = document.querySelector('.underlay');
-let form = document.querySelector('.form');
-let formName = form.querySelector('#name');
-let formTel = form.querySelector('#tel');
-let formTextarea = form.querySelector('#text');
-let formButton = form.querySelector('.form__button');
-let modal = document.querySelector('.modal');
-let modalForm = modal.querySelector('.modal__inputs-container');
-let modalClose = modal.querySelector('.modal__button--close');
-let modalSubmit = modal.querySelector('.modal__button--submit')
-let modalName = modal.querySelector('#modal-name');
-let modalTel = modal.querySelector('#modal-tel');
-let modalTextarea = modal.querySelector('#modal-text');
+var popupButton = document.querySelector('.header__user-button');
+var modalUnderlay = document.querySelector('.underlay');
+var form = document.querySelector('.form');
+if (form) {
+  var formName = form.querySelector('#name');
+  var formTel = form.querySelector('#tel');
+  var formTextarea = form.querySelector('#text');
+  var formButton = form.querySelector('.form__button');
+}
 
-/********************************************/
-  // Управление модальным окном 
-/********************************************/
+var modal = document.querySelector('.modal');
+
+if (modal) {
+  var modalForm = modal.querySelector('.modal__inputs-container');
+  var modalClose = modal.querySelector('.modal__button--close');
+  var modalSubmit = modal.querySelector('.modal__button--submit');
+  var modalName = modal.querySelector('#modal-name');
+  var modalTel = modal.querySelector('#modal-tel');
+  var modalTextarea = modal.querySelector('#modal-text');
+}
 
 function onEscPress(evt) {
   if (evt.code === KeyCodes.ESC) {
@@ -39,10 +41,14 @@ function onEscPress(evt) {
 function openPopup() {
   if (modalUnderlay.classList.contains('hidden')) {
     modalUnderlay.classList.remove('hidden');
+    if (!document.body.classList.contains('no-scrolling')) {
+      document.body.classList.add('no-scrolling');
+    }
+
     document.addEventListener('keydown', onEscPress);
 
-    let telStorage = localStorage.getItem("tel");
-    let nameStorage = localStorage.getItem("name");
+    var telStorage = localStorage.getItem('tel');
+    var nameStorage = localStorage.getItem('name');
 
     if (nameStorage) {
       modalName.value = nameStorage;
@@ -61,6 +67,10 @@ function openPopup() {
 function closePopup() {
   if (!modalUnderlay.classList.contains('hidden')) {
     modalUnderlay.classList.add('hidden');
+    if (document.body.classList.contains('no-scrolling')) {
+      document.body.classList.remove('no-scrolling');
+    }
+
     document.removeEventListener('keydown', onEscPress);
   }
 }
@@ -70,124 +80,131 @@ function onPopupButtonClick() {
 }
 
 function onModalUnderlayClick(evt) {
-  if(evt.target === modalClose || evt.target === modalUnderlay) {
+  if (evt.target === modalClose || evt.target === modalUnderlay) {
     closePopup();
   }
 }
 
-popupButton.addEventListener('click', onPopupButtonClick);
+if (popupButton) {
+  popupButton.addEventListener('click', onPopupButtonClick);
+}
 
-modalUnderlay.addEventListener('click', function(evt) {
-  onModalUnderlayClick(evt);
-});
+if (modalUnderlay) {
+  modalUnderlay.addEventListener('click', function (evt) {
+    onModalUnderlayClick(evt);
+  });
+}
 
-/********************************************/
-  // Проверка валидности телефона и маска ввода
-/********************************************/
 function onTelInputChange(key, input) {
-    const digitsReg = /^[0-9]$/; 
+  var digitsReg = /^[0-9]$/;
 
-    if (!key.match(digitsReg) || input.value.length === 14) {
-      return;
-    };
+  if (!key.match(digitsReg) || input.value.length === 14) {
+    return;
+  }
 
-    input.value += key;
+  input.value += key;
 
-    if (input.value.length === 6) {
-      input.value += ')';
-    }   
+  if (input.value.length === 6) {
+    input.value += ')';
+  }
 }
 
 function onTelInputFocus(input) {
-  const telStartPattern = "+7("
+  var telStartPattern = '+7(';
   input.value = telStartPattern;
 
   function onInputKeypress(evt) {
     evt.preventDefault();
-    let key = evt.key;
+    var key = evt.key;
     onTelInputChange(key, input);
   }
 
   input.addEventListener('keypress', onInputKeypress);
 
-  input.addEventListener('blur', function() {
+  input.addEventListener('blur', function () {
     input.removeEventListener('keypress', onInputKeypress);
-  })
-}
-
-modalTel.addEventListener('focus', function() {
-  onTelInputFocus(modalTel);
-})
-
-formTel.addEventListener('focus', function() {
-  onTelInputFocus(formTel);
-})
-
-function validate(input) {
-  if(input.value.length < TEL_LENGTH) {
-    input.setCustomValidity('Телефонный номер должен состоять из 10 цифр')
-  } else {
-    input.setCustomValidity('')
-  }
-}
-
-formButton.addEventListener('click', function() {
-  validate(formTel);
-});
-
-modalSubmit.addEventListener('click', function() {
-  validate(modalTel);
-});
-
-form.addEventListener('submit', function(evt) {
-  localStorage.setItem("name", formName.value);
-  localStorage.setItem("tel", formTel.value);
-  localStorage.setItem("text", formTextarea.value);
-}); 
-
-modalForm.addEventListener('submit', function(evt) {
-  localStorage.setItem("name", modalName.value);
-  localStorage.setItem("tel", modalTel.value);
-  localStorage.setItem("text", modalTextarea.value);
-}); 
-
-/********************************************/
-  // "Аккордион в подвале"
-/********************************************/
-
-let footer = document.querySelector('.footer');
-let footerBlocks = Array.prototype.slice.call(footer.querySelectorAll('.footer__nav, .footer__contacts'));
-
-function toggleVisibility(element) {
-  let parent = element.parentNode;
-  parent.classList.toggle('folded');
-}
-
-function hideBlocks() {
-  footerBlocks.forEach(function(block) {
-    block.classList.add('folded');
   });
 }
 
-function showBlocks() {
-  footerBlocks.forEach(function(block) {
-    block.classList.remove('folded');
+if (modalTel) {
+  modalTel.addEventListener('focus', function () {
+    onTelInputFocus(modalTel);
+  });
+}
+
+if (formTel) {
+  formTel.addEventListener('focus', function () {
+    onTelInputFocus(formTel);
+  });
+}
+
+function validate(input) {
+  if (input.value.length < TEL_LENGTH) {
+    input.setCustomValidity('Телефонный номер должен состоять из 10 цифр');
+  } else {
+    input.setCustomValidity('');
+  }
+}
+
+if (formButton) {
+  formButton.addEventListener('click', function () {
+    validate(formTel);
+  });
+}
+
+if (modalSubmit) {
+  modalSubmit.addEventListener('click', function () {
+    validate(modalTel);
+  });
+}
+
+if (form) {
+  form.addEventListener('submit', function () {
+    localStorage.setItem('name', formName.value);
+    localStorage.setItem('tel', formTel.value);
+    localStorage.setItem('text', formTextarea.value);
+  });
+}
+
+if (modalForm) {
+  modalForm.addEventListener('submit', function () {
+    localStorage.setItem('name', modalName.value);
+    localStorage.setItem('tel', modalTel.value);
+    localStorage.setItem('text', modalTextarea.value);
+  });
+}
+
+var footerTop = document.querySelector('.footer--top');
+if (footerTop) {
+  var footerBlocks = Array.prototype.slice.call(footerTop.querySelectorAll('.footer__nav, .footer__contacts'));
+}
+
+function toggleVisibility() {
+  footerBlocks.forEach(function (block) {
+    block.classList.toggle('folded');
   });
 }
 
 function checkResolution() {
-    return document.documentElement.clientWidth > Resolutions.MOBILE_MAX
+  return document.documentElement.clientWidth > Resolutions.MOBILE_MAX;
 }
 
-footer.addEventListener('click', function(evt) {
-  let target = evt.target.closest('h3');
+if (footerTop) {
+  footerTop.addEventListener('click', function (evt) {
+    var target = evt.target.closest('h3');
 
-  if(!target) return;
-  if(checkResolution()) return
+    if (!target) {
+      return;
+    }
 
-  toggleVisibility(target);
-})
+    if (checkResolution()) {
+      return;
+    }
 
-if (!checkResolution()) {
-  hideBlocks();
+    toggleVisibility();
+  });
+}
+
+if (!checkResolution() && footerBlocks) {
+  footerBlocks[0].classList.add('folded');
 }
